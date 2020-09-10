aa8c70f77f5bce81ac6cd9c937124f44954af55f
mateusbatista4 <49661647+mateusbatista4@users.noreply.github.com>
2020-09-10 19:32:16 -0300

    Images

--- backend/urls.py
+++ backend/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 from rest_framework import routers
 from core.views import MemberViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'members', MemberViewSet )
@@ -25,4 +27,4 @@ router.register(r'members', MemberViewSet )
 urlpatterns = [
     path('',include(router.urls)),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
